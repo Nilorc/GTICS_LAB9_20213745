9ab54798fc6c8bf89990291f4a01bd3ede62573e
NiloRC <145509554+Nilorc@users.noreply.github.com>
2024-11-11 19:29:42 -0500

    Creacion inicial del proyecto

---
+++ lab9_2021345/src/main/java/com/example/lab9_2021345/Lab92021345Application.java
@@ -0,0 +1,13 @@
+package com.example.lab9_2021345;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab92021345Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab92021345Application.class, args);
+    }
+
+}
---
+++ lab9_2021345/src/test/java/com/example/lab9_2021345/Lab92021345ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab9_2021345;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab92021345ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
